eae0c672eacbd3de3ee26c7331b618f468b721cf
Collins Muriuki <hello@collinsmuriuki.xyz>
2026-02-20 13:43:03 +0300

    chore: Add deploy ci/wf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://mwananchi.tech",
+  site: "https://mwananchi-tech.github.io",
   markdown: {
     shikiConfig: {
       theme: "github-dark",
